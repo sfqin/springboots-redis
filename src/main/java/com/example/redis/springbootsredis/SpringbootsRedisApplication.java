a531587598e00b2a5156fe1956a9cb6e77715fd1
fidoqin <fidoqin@wesure.cn>
2020-01-22 16:55:35 +0800

    init project

---
+++ src/main/java/com/example/redis/springbootsredis/SpringbootsRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.redis.springbootsredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redis/springbootsredis/SpringbootsRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis.springbootsredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
